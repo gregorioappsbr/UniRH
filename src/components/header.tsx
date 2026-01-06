8b2e7959aae6e02f7ce4a6bdb748233e050d2940
GREGORIO APP <gregorio.apps.br@gmail.com>
2026-01-06 14:38:46 +0000

    diminue um pouco (_for element h1_)

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,7 +5,7 @@ export function Header() {
     <header className="flex items-center justify-center py-4">
       <div className="flex items-center gap-3">
         <ScrollText className="h-18 w-18 text-primary" />
-        <h1 className="text-6xl font-bold text-foreground">UniRH</h1>
+        <h1 className="text-5xl font-bold text-foreground">UniRH</h1>
       </div>
     </header>
   );
