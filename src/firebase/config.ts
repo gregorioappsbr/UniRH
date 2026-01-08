a858dc83d144be31b407c6bba2dab067cb5c09e3
GREGORIO APP <gregorio.apps.br@gmail.com>
2026-01-08 00:44:13 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7506943265-d71cb",
+  "appId": "1:265491276128:web:55df32f74e039ed2043e05",
+  "apiKey": "AIzaSyBrD8BvPKoepocsi_yTjkOGK-GsbCK7XLo",
+  "authDomain": "studio-7506943265-d71cb.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "265491276128"
+};
