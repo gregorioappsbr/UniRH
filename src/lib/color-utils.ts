bd759408e63be15164382948be01b8970fba3f78
GREGORIO APP <gregorio.apps.br@gmail.com>
2026-01-09 14:08:43 +0000

    porem ficou transparente e quero cor solida

--- src/lib/color-utils.ts
+++ src/lib/color-utils.ts
@@ -1,26 +1,26 @@
 
 const feminineColors = [
-  'bg-rose-200/50 dark:bg-rose-800/10',
-  'bg-pink-200/50 dark:bg-pink-800/10',
-  'bg-fuchsia-200/50 dark:bg-fuchsia-800/10',
-  'bg-purple-200/50 dark:bg-purple-800/10',
-  'bg-violet-200/50 dark:bg-violet-800/10',
+  'bg-rose-200 dark:bg-rose-800/20',
+  'bg-pink-200 dark:bg-pink-800/20',
+  'bg-fuchsia-200 dark:bg-fuchsia-800/20',
+  'bg-purple-200 dark:bg-purple-800/20',
+  'bg-violet-200 dark:bg-violet-800/20',
 ];
 
 const masculineColors = [
-  'bg-blue-200/50 dark:bg-blue-800/10',
-  'bg-green-200/50 dark:bg-green-800/10',
-  'bg-cyan-200/50 dark:bg-cyan-800/10',
-  'bg-teal-200/50 dark:bg-teal-800/10',
-  'bg-indigo-200/50 dark:bg-indigo-800/10',
+  'bg-blue-200 dark:bg-blue-800/20',
+  'bg-green-200 dark:bg-green-800/20',
+  'bg-cyan-200 dark:bg-cyan-800/20',
+  'bg-teal-200 dark:bg-teal-800/20',
+  'bg-indigo-200 dark:bg-indigo-800/20',
 ];
 
 const neutralColors = [
-  'bg-yellow-200/50 dark:bg-yellow-800/10',
-  'bg-orange-200/50 dark:bg-orange-800/10',
-  'bg-amber-200/50 dark:bg-amber-800/10',
-  'bg-lime-200/50 dark:bg-lime-800/10',
-  'bg-sky-200/50 dark:bg-sky-800/10',
+  'bg-yellow-200 dark:bg-yellow-800/20',
+  'bg-orange-200 dark:bg-orange-800/20',
+  'bg-amber-200 dark:bg-amber-800/20',
+  'bg-lime-200 dark:bg-lime-800/20',
+  'bg-sky-200 dark:bg-sky-800/20',
 ];
 
 const allColors = [...feminineColors, ...masculineColors, ...neutralColors];
